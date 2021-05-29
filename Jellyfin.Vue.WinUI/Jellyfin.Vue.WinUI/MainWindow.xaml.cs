2cb577262adb11551c69914a32c60c6e26683cd8
crobibero <cody@robibe.ro>
2021-05-29 07:42:11 -0600

    init

---
+++ Jellyfin.Vue.WinUI/Jellyfin.Vue.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.IO;
+
+namespace Jellyfin.Vue.WinUI
+{
+    /// <summary>
+    /// The main window.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            await WebView.EnsureCoreWebView2Async();
+            WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "jellyfin.winui.local",
+                "Client",
+                Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow);
+            WebView.Source = new Uri("http://jellyfin.winui.local/index.html");
+
+            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        }
+
+        private void CoreWebView2_NavigationStarting(Microsoft.Web.WebView2.Core.CoreWebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
+        {
+            var extension = System.IO.Path.GetExtension(args.Uri);
+            if (string.IsNullOrEmpty(extension))
+            {
+                sender.Navigate(args.Uri.TrimEnd('/') + "/index.html");
+            }
+        }
+    }
+}
