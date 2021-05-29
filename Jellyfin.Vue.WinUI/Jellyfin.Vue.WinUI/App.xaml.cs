26cbe06ce6063c62764c8bdb1fddd432d88148b4
crobibero <cody@robibe.ro>
2021-05-29 10:52:33 -0600

    prep for store

--- Jellyfin.Vue.WinUI/Jellyfin.Vue.WinUI/App.xaml.cs
+++ Jellyfin.Vue.WinUI/Jellyfin.Vue.WinUI/App.xaml.cs
@@ -1,20 +1,7 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Serilog;
+using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -40,8 +27,15 @@ namespace Jellyfin.Vue.WinUI
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
+            var storagePath = ApplicationData.Current.LocalFolder.Path;
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    Path.Combine(storagePath, "log", "jellyfin.vue.winui..log"),
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             m_window = new MainWindow();
             m_window.Activate();
         }
--- Jellyfin.Vue.WinUI/Jellyfin.Vue.WinUI/MainWindow.xaml.cs
+++ Jellyfin.Vue.WinUI/Jellyfin.Vue.WinUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Shapes;
+using Microsoft.Web.WebView2.Core;
+using Serilog;
 using System;
 using System.IO;
 
@@ -12,25 +13,35 @@ namespace Jellyfin.Vue.WinUI
     {
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Title = "Jellyfin Vue";
             InitializeAsync();
         }
 
         private async void InitializeAsync()
         {
-            await WebView.EnsureCoreWebView2Async();
-            WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
-                "jellyfin.winui.local",
-                "jellyfin-vue",
-                Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow);
-            WebView.Source = new Uri("http://jellyfin.winui.local/index.html");
+            try
+            {
+                await WebView.EnsureCoreWebView2Async();
+                // Content files are in the parent directory.
+                WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                    "jellyfin.winui.local",
+                    "../jellyfin-vue",
+                    CoreWebView2HostResourceAccessKind.Allow);
+                WebView.Source = new Uri("http://jellyfin.winui.local/index.html");
 
-            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+                WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            }
+            catch(Exception ex)
+            {
+                Log.Logger.Error(ex, "Error initializing router");
+                throw;
+            }
         }
 
-        private void CoreWebView2_NavigationStarting(Microsoft.Web.WebView2.Core.CoreWebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
+        private void CoreWebView2_NavigationStarting(CoreWebView2 sender, CoreWebView2NavigationStartingEventArgs args)
         {
-            var extension = System.IO.Path.GetExtension(args.Uri);
+            var extension = Path.GetExtension(args.Uri);
             if (string.IsNullOrEmpty(extension))
             {
                 sender.Navigate(args.Uri.TrimEnd('/') + "/index.html");
